b22ed300fb1090892f74b1dea16a86a6d13075ee
ivand1988 <ivand1988@outlook.com>
2022-06-15 17:07:01 +0300

    Added settings env + gunicorn

--- db.py
+++ db.py
@@ -3,8 +3,12 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import sqlalchemy
 from databases import Database
+from settings import Settings
 
-DATABASE_URL = "sqlite:///./test.db"
+settings = Settings()
+
+
+DATABASE_URL = f"sqlite:///./{settings.database_url}"
 # DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
 database = Database(DATABASE_URL)
